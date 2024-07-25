5023313a009d82a841598e68886cc5391bb5fbaa
Boris Ghidaglia <borisghidaglia@gmail.com>
2024-07-25 20:43:04 +0200

    Adds aws credentials from env

--- app/_db/Client.ts
+++ app/_db/Client.ts
@@ -1,5 +1,11 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-export const client = new DynamoDBClient({ region: "us-east-1" });
+export const client = new DynamoDBClient({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
 export const docClient = DynamoDBDocumentClient.from(client);
