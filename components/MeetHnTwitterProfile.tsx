736a0385a0dce40bbef43188ac5703d9c7d3a4ca
Boris Ghidaglia <borisghidaglia@gmail.com>
2024-08-15 12:22:41 +0200

    Adds twitter-banner and twitter-pp

---
+++ components/MeetHnTwitterProfile.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import { GeistSans } from "geist/font/sans";
+
+export function TwitterBanner() {
+  return (
+    <div className="grid h-[500px] w-[1500px] place-items-center border">
+      <h1 className="flex items-baseline gap-2 text-9xl font-bold">meet.hn</h1>
+    </div>
+  );
+}
+
+export function TwitterProfilePicture() {
+  return (
+    <div
+      className={cn(
+        "grid h-[400px] w-[400px] place-items-center border bg-[#ff6602] text-white",
+        GeistSans.className,
+      )}
+    >
+      <h1 className="flex items-baseline gap-2 text-[15rem] font-semibold leading-none">
+        M
+      </h1>
+    </div>
+  );
+}
