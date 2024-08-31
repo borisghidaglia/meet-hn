968a7fded48e686153084ca838c776d4cf877fbe
Boris Ghidaglia <borisghidaglia@gmail.com>
2024-08-31 22:11:57 +0200

    Switch to CSS header to fix layout shift

--- app/@header/page.tsx
+++ app/@header/page.tsx
@@ -5,11 +5,13 @@ import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-import { useMediaQuery } from "@/app/hooks/useMediaQuery";
 import { ExternalLink } from "@/components/ui/ExternalLink";
 
 import logo from "@/public/logo.svg";
 
+// Fixes:
+// useSearchParams() should be wrapped in a suspense boundary at page "/".
+// Read more: https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
 export default function SuspensedHeader() {
   return (
     <Suspense>
@@ -22,62 +24,26 @@ function Header() {
   const searchParams = useSearchParams();
   const city = searchParams.get("city");
   const pathname = usePathname();
-  const isDesktop = useMediaQuery("(min-width: 768px)");
-
-  if (isDesktop)
-    return (
-      <header>
-        <div className="flex items-baseline justify-between">
-          <h1 className="flex items-baseline gap-2 text-3xl font-bold">
-            <Link href="/" className="contents">
-              <Image src={logo} alt="meet.hn logo" width="40" height="40" />
-              meet.hn
-            </Link>
-          </h1>
-          <ul className="flex gap-1 text-sm md:gap-2 md:text-base">
-            {city || pathname !== "/" ? (
-              <>
-                <li>
-                  <Link href="/" className="hover:underline">
-                    Register
-                  </Link>
-                </li>
-                <li>·</li>
-              </>
-            ) : null}
-            <li>
-              <Link href="/feedback" className="hover:underline">
-                Feedback
-              </Link>
-            </li>
-            <li>·</li>
-            <li>
-              <Link href="/delete" className="hover:underline">
-                Delete
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <p className="text-sm text-gray-400">
-          by{" "}
-          <ExternalLink href="https://x.com/borisfyi">@borisfyi</ExternalLink>
-        </p>
-      </header>
-    );
 
   return (
-    <header className="flex flex-col gap-2">
+    <header className="grid grid-cols-1 grid-rows-2 items-baseline gap-2 md:grid-cols-2 md:grid-rows-2">
       <h1 className="flex items-baseline gap-2 text-3xl font-bold">
         <Link href="/" className="contents">
           <Image src={logo} alt="meet.hn logo" width="40" height="40" />
           meet.hn
         </Link>
       </h1>
-      <ul className="flex gap-2 text-base">
+      <p className="hidden text-sm text-gray-400 md:row-start-2 md:inline-block">
+        by <ExternalLink href="https://x.com/borisfyi">@borisfyi</ExternalLink>
+      </p>
+      <ul className="flex gap-2 text-base md:justify-self-end">
         {city || pathname !== "/" ? (
           <>
             <li>
-              <Link href="/" className="underline underline-offset-2">
+              <Link
+                href="/"
+                className="underline underline-offset-2 md:no-underline"
+              >
                 Register
               </Link>
             </li>
@@ -85,13 +51,19 @@ function Header() {
           </>
         ) : null}
         <li>
-          <Link href="/feedback" className="underline underline-offset-2">
+          <Link
+            href="/feedback"
+            className="underline underline-offset-2 md:no-underline"
+          >
             Feedback
           </Link>
         </li>
         <li>·</li>
         <li>
-          <Link href="/delete" className="underline underline-offset-2">
+          <Link
+            href="/delete"
+            className="underline underline-offset-2 md:no-underline"
+          >
             Delete
           </Link>
         </li>
